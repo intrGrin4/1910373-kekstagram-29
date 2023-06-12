11e7e527d776d7f97301b7b16e94b0f9a9c62f72
Alexander Trofimov <Sasha1233515744@gmail.com>
2023-06-12 20:38:36 +0300

    сделал доп функцию с проверкой NaN

--- js/functions.js
+++ js/functions.js
@@ -13,6 +13,17 @@ checkingLength('проверяемая строка', 10);
 const checkingPalindrome = (line) => {
   line = line.toString().toLowerCase().replace(/\s|[,.?!"/-]/g, ''); // приравниваем к нижнему регистру, используем regEx: находим все пробелы и заменяем их. Для чисел используем метод toString.
   return line === line.split('').reverse().join(''); // делим слово по-символьно и меняем местасми
-
 };
 checkingPalindrome(12321);
+
+//Функция проверяет, есть ли числа в строке
+const checkingNumbers = (num) => {
+  const result = num.replace(/[^0-9]/g, '');
+  const numbersNan = Number(num);
+
+  if (result) {
+    return result;
+  }
+  return numbersNan;
+};
+checkingNumbers('1 кефир, 0.5 батона');
