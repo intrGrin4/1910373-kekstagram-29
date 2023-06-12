ccf08365fd887f7e0433ce22a69e651d420ec19b
Alexander Trofimov <Sasha1233515744@gmail.com>
2023-06-12 23:51:57 +0300

    исправил functions.js после созвона

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,22 @@
 // Функция для проверки длины строки
 
-const checkingLength = (line, numbers) => {
-  const result = (line.length <= numbers);
-  return result;
-};
-checkingLength('проверяемая строка', 20);
-checkingLength('проверяемая строка', 18);
-checkingLength('проверяемая строка', 10);
+const checkLengthString = (string, number) => string.length <= number;
+checkLengthString('проверяемая строка', 20);
+checkLengthString('проверяемая строка', 18);
+checkLengthString('проверяемая строка', 10);
 
 // Функция для проверки палиндрома
 
-const checkingPalindrome = (line) => {
-  line = line.toString().toLowerCase().replace(/\s|[,.?!"/-]/g, ''); // приравниваем к нижнему регистру, используем regEx: находим все пробелы и заменяем их. Для чисел используем метод toString.
-  return line === line.split('').reverse().join(''); // делим слово по-символьно и меняем местасми
+const checkPalindromeString = (string) => {
+  string = string.toString().toLowerCase().replaceAll(' ', ''); // приравниваем к строке, к нижнему регистру, заменяем все пробелы
+  return string === string.split('').reverse().join(''); // преобразуем в массив, делим слово по-символьно, меняем местасми и преобразуем в строку
 };
-checkingPalindrome(12321);
+checkPalindromeString(12321);
 
-//Функция проверяет, есть ли числа в строке
-const checkingNumbers = (num) => {
-  const result = num.replace(/[^0-9]/g, '');
-  const numbersNan = Number(num);
+//Функция проверяет есть ли числа в строке
 
-  if (result) {
-    return result;
-  }
-  return numbersNan;
+const checkExtractDigits = (number) => {
+  const result = number.replace(/[^0-9]/g, '');
+  return parseInt(result, 10);
 };
-checkingNumbers('1 кефир, 0.5 батона');
+checkExtractDigits('кефир, батона');
