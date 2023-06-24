2bf0a438aa2b5c820c5ecd14595814c84a929f9c
Alexander Trofimov <Sasha1233515744@gmail.com>
2023-06-24 21:53:20 +0300

    Разбил файлы на модули

---
+++ js/util.js
@@ -0,0 +1,4 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomArrayElement = (elements) => elements[getRandomNumber (0, elements.length - 1)];
+
+export {getRandomArrayElement, getRandomNumber};
